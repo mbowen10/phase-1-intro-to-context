b1995caa9c5725285ce0dc0a655cd3670edafe4a
Michael Bowen <michaelbowen1092@gmail.com>
2022-03-22 11:30:06 -0400

    commit tree 41f470fd2a9ba99ced0714f46c75a30ba272a8af

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+// function createEmployeeRecord(firstName, familyName, title, payPerHour){
+//   this.firstName = firstName;
+//   this.familyName = familyName;
+//   this.title = title;
+//   this.payPerHour = payPerHour;
+
+//   return `${firstName} ${familyName} ${title} ${payPerHour}`
+// }
+// console.log(createEmployeeRecord)
+
+
